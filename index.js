c60b875c1fa8527791470aa4908cef4e6c30f74e
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-11-17 22:27:59 -0500

    commit tree 27ec63d8ce9eacb4e7460eb626ff0c78c5a8061e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    loadedDOM()
+});
+
+function loadedDOM() {
+    document.getElementById("text")
+    text.textContent = "This is really cool!"
+}
